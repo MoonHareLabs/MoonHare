30fe1cc11a68f1b61fb5b7cd5b3651d3d579b26f
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-07-01 22:56:13 +0530

    Update classes.js

--- src/classes.js
+++ src/classes.js
@@ -1,7 +1,17 @@
+CLASSRE = /\s+class\s*=\s*"((?:\\["\\]|[^"\\])*)"/gm
+
 export function getNames(input) {
     return input.match(/[^<>"'`\s]*[^<>"'`\s:]/g)
 }
 
 export function getHTMLClasses(input) {
-    class="((?:\\["\\]|[^"\\])*)"
+    let matches = [],
+        match = CLASSRE.exec(input)[1];
+
+    while (match) {
+        matches.push.apply(match, match.split(/\s*|\n/))
+        match = CLASSRE.exec(input)[1]
+    }
+
+    return matches
 }
