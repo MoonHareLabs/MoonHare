265b4ae727c52c8cf14336fc927553b9f3b0c1c3
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-07-03 17:58:34 +0530

    Update processVariants.js

--- src/processVariants.js
+++ src/processVariants.js
@@ -1,5 +1,17 @@
-function processVariants() {
-    varaintNames = className.split(':')
-    pluginPart = classNames.pop()
-    varaintNames.forEach(variant => sheet[])
+function process() {
+    let varaintNames, pluginPart, styleSheet, varaintName, variant
+    varaintNames = className.split(config.variantsSeparator)
+    pluginPart = varaintNames.pop()
+    if (config.prefix) {
+        if (pluginPart.startsWith(config.prefix)) pluginPart = pluginPart.slice(config.prefix.length)
+        else return
+    }
+    pluginArgs = pluginPart.split(config.separator)
+    
+    for (varaintName of varaintNames) {
+        variant = config.variants[varaintName]
+        variant.call()
+        if (!styleSheet.has(varaintName)) styleSheet.set(varaintName, new Map())
+        styleSheet = styleSheet.get(varaintName)
+    }
 }
