d9c5b738a7fd9cd8423ffc8450eecb2c6f44c292
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-07-04 12:23:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ class Processor {
         let variantName, variant, styleSheet
         for (variantName of variantNames) {
             variant = this.config.variants[variantName]
-            variant.call(this)
+            variant.call(this, styles)
             if (!styleSheet.has(variantName)) styleSheet.set(variantName, new Map())
             styleSheet = styleSheet.get(variantName)
         }
@@ -30,7 +30,7 @@ class Processor {
     processPlugins(pluginPart, className) {
         let params, pluginName, plugin
         params = pluginPart.split(this.config.separator)
-        pluginName = pluginPart.shift()
+        pluginName = params.shift()
         plugin = this.config.plugins[pluginName]
         if (plugin) return plugin.call(this, { params: params, raw: params.join(this.config.separator) })
         if (!this.config.skipWords) console.log(`Unsupported plugin name('${pluginName}') in '${className}'`)
