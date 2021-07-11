0b897f0f3a7a0a69133da8c09e3749707de1bba2
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-07-11 16:41:34 +0530

    Update style.js

--- src/style.js
+++ src/style.js
@@ -2,6 +2,22 @@ class Layer {
     layers: [base, utilities, components]
 }
 
+export class Style {
+    meta = { type: 'components', group: 'plugin', order: 0, offset: 0, corePlugin: false };
+    selector = [];
+    important = [];
+    property = [];
+    atRules = [];
+    pseudoClasses = [];
+    pseudoElements = [];
+    parentSelectors = [];
+    childSelectors = [];
+    brotherSelectors = [];
+    wrapProperties = [];
+    wrapSelectors = [];
+    wrapRules = [];
+}
+
 class StyleSheet {
     layers: [base, utilities, components]
 }
