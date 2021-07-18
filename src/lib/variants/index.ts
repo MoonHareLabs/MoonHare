058808e6e50d7d08c3787c313c71391474e79bf9
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-07-18 19:00:46 +0530

    Add files via upload

---
+++ src/lib/variants/index.ts
@@ -0,0 +1,26 @@
+import { generateScreens } from './screen';
+import { generateThemes } from './theme';
+import { generateStates } from './state';
+
+import type { Style } from '../../utils/style';
+import type { BaseTheme, Config, DictStr } from '../../interfaces';
+
+export type Variants = {
+  screen: { [key: string]: () => Style };
+  theme: { [key: string]: () => Style };
+  state: { [key: string]: () => Style };
+}
+
+export function resolveVariants(config: Config): Variants {
+  return {
+    screen: generateScreens(((config.theme as BaseTheme)?.screens ?? {}) as DictStr),
+    theme: generateThemes(config.darkMode),
+    state: generateStates(config.variantOrder ?? []),
+  };
+}
+
+export {
+  generateScreens,
+  generateThemes,
+  generateStates,
+};
