db7e03f971e1633d6a6257c220abfee2ac77e4eb
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-07-19 20:08:45 +0530

    Update vite.config.ts

--- test/plugin/typography/vite.config.ts
+++ test/plugin/typography/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import windi from 'vite-plugin-windicss';
+import mh from 'vite-plugin-moonhare';
 import markdown from 'vite-plugin-md';
 import vue from '@vitejs/plugin-vue';
 
@@ -9,7 +9,7 @@ export default defineConfig({
     vue({
       include: [/\.vue$/, /\.md$/],
     }),
-    windi(),
+    mh(),
     markdown(),
   ],
 });
